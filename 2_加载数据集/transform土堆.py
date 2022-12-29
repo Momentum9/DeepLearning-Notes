d57aebc29057c1bd99eb81d30256cbdbc88a52fa
sys <992045294@qq.com>
2022-12-29 21:09:50 +0800

    just for synchronizing

--- 2_加载数据集/transform土堆.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-from torchvision import transforms
-
-img_pth = "data/hymenoptera_data/train/ants/6240329_72c01e663e.jpg"
-img = Image.open(img_pth)
-tensor_trans = transforms.ToTensor()
-img_tensor = tensor_trans(img)
-print(img_tensor.shape)
